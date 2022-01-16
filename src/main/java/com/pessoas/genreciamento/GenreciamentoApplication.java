bb07e38bf14ad47c245946e679b8ff1c3602b3b5
/Jeronimo <jeronimo_espala@hotmail.com>
2022-01-16 11:28:51 -0300

    Dockerfile

---
+++ src/main/java/com/pessoas/genreciamento/GenreciamentoApplication.java
@@ -0,0 +1,13 @@
+package com.pessoas.genreciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenreciamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenreciamentoApplication.class, args);
+	}
+
+}
