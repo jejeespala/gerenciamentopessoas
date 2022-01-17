330437164ce005019381612cfdbce391adb3de8c
/Jeronimo <jeronimo_espala@hotmail.com>
2022-01-17 20:08:47 -0300

    Heroku att

--- src/main/java/com/pessoas/genreciamento/controller/PessoaController.java
+++ src/main/java/com/pessoas/genreciamento/controller/PessoaController.java
@@ -1,12 +1,14 @@
 package com.pessoas.genreciamento.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/vai")
 public class PessoaController {
 
-    @GetMapping("/vai")
+    @GetMapping
     public String message(){
 
         return "Oi Pessoal";
